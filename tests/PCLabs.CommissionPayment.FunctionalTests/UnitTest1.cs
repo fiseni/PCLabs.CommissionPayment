18e798b62f078f35d5e824257c2c26677bcd2bb7
Fati Iseni <fati.iseni@pozitrongroup.com>
2022-02-14 17:52:34 +0100

    Added test projects.

---
+++ tests/PCLabs.CommissionPayment.FunctionalTests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace PCLabs.CommissionPayment.FunctionalTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ tests/PCLabs.CommissionPayment.UnitTests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace PCLabs.CommissionPayment.UnitTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
